efdc5077cb9ec8792680b8e21166093b46e24cd4
Yuting Wu <wu000244@umn.edu>
2020-02-12 18:42:02 -0600

    roman finished

--- digits to roman .py
+++ digits to roman .py
@@ -6,12 +6,12 @@ def rotode(intr):
             if intr == ro:
                 stri = dict1[ro]
                 return stri
-    else:
+    elif intr > 10 and intr < 40:
         intr -= 10
         for ro in dict1:
             if intr <= 10:
                 stri = dict1[intr]
-                return 'X' + stri
+                return 'X' + stri # for 20 and above 
             else:
                 if intr >10 and intr < 20: 
                     stri = dict1[intr]
@@ -19,8 +19,31 @@ def rotode(intr):
                 elif intr >= 20 and intr < 30:
                     intr -= 20
                     stri = dict1[intr]
-                    return 'XXX' + stri
-    # before 31, new trend for number above 30. 
+                    return 'XXX' + stri # for 30 and above 
+    elif intr >= 40:
+        return above40(intr)
+    elif intr == 100:
+        return 'C' 
+def above40(intr):
+    dict2 = {40:"XL" , 50:"L", 60: "LX", 70: "LXX", 80: "LXXX", 90: "XC"}
+    str2 = '' # single digit
+    if intr >= 40:
+        for ro in dict2:
+            str3 = intr - ro
+            if str3 < 10:
+                single = rotode(str3)
+                str2 = dict2[ro]
+                call = str2 + single
+                return call
+            
+            
+            
+        
+
+    
+    
+
+
             
             
         
