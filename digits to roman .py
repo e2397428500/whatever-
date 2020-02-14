b134920682203a06d5fbf504a145bfd625f0508e
Yuting Wu <wu000244@umn.edu>
2020-02-13 18:38:21 -0600

    Update digits to roman .py
    1 to 1000 digits to roman.py

--- digits to roman .py
+++ digits to roman .py
@@ -1,54 +1,51 @@
-def rotode(intr):
-    dict1 = {0:'',1:"I", 2:"II", 3:"III", 4:"IV", 5: "V", 6:"VI", 7: 'VII', 8:'VIII', 9: 'IX', 10: 'X'}# from one to 30.
+## this function convert digit to roman number from range(1 to 1000)
+
+
+def digits(intr):
+    if intr < 10:
+        return single(intr)
+    if intr >= 10 and intr < 100:
+        return aboveten(intr)
+    if intr >= 100 and intr <= 1000:
+        return abovehun(intr) 
+
+def single(intr):
+    dict1 = {0:'',1:"I", 2:"II", 3:"III", 4:"IV", 5: "V", 6:"VI", 7: 'VII', 8:'VIII', 9: 'IX'}# from one to 30. SINGLE DITS FUNCTION
     stri = ''
-    if intr <= 10:
-        for ro in dict1:
-            if intr == ro:
-                stri = dict1[ro]
-                return stri
-    elif intr > 10 and intr < 40:
-        intr -= 10
-        for ro in dict1:
-            if intr <= 10:
-                stri = dict1[intr]
-                return 'X' + stri # for 20 and above 
-            else:
-                if intr >10 and intr < 20: 
-                    stri = dict1[intr]
-                    return 'XX' + stri
-                elif intr >= 20 and intr < 30:
-                    intr -= 20
-                    stri = dict1[intr]
-                    return 'XXX' + stri # for 30 and above 
-    elif intr >= 40:
-        return above40(intr)
-    elif intr == 100:
-        return 'C' 
-def above40(intr):
-    dict2 = {40:"XL" , 50:"L", 60: "LX", 70: "LXX", 80: "LXXX", 90: "XC"}
+    for ro in dict1:
+        if intr == ro:
+            stri = dict1[ro]
+            return stri
+def aboveten(intr):
+    dict2 = {10:"X", 20: "XX", 30:"XXX", 40:"XL" , 50:"L", 60: "LX", 70: "LXX", 80: "LXXX", 90: "XC"}
     str2 = '' # single digit
-    if intr >= 40:
-        for ro in dict2:
-            str3 = intr - ro
-            if str3 < 10:
-                single = rotode(str3)
-                str2 = dict2[ro]
-                call = str2 + single
-                return call
-            
-            
-            
+    for ro in dict2:
+        str3 = intr - ro
+        if str3 < 10:
+            sin= single(str3)
+            str2 = dict2[ro]
+            call = str2 + sin
+            return call
+
+def abovehun(intr):
+    dict3 = {100:"C",200:"CC", 300: "CCC", 400:"CD", 500: "D", 600: "DC", 700: "DCC", 800: "DCCC", 900: "CM", 1000: "M"}
+    str1 = ''
+    for ro in dict3:
+        str2 = intr - ro
+        if str2 < 100 and str2 >= 10:
+            sin = aboveten(str2)
+            str1 = dict3[ro]
+            call = str1 + sin
+            return call
+        elif str2 < 100 and str2 <= 10:
+            sin = single(str2)
+            str1 = dict3[ro]
+            call = str1 + sin
+            return call
         
-
     
-    
-
-
-            
-            
-        
-  
-
+##call = int(input("Pls enter a integer from 1 to 1000: "))
+##print(digits(call))
 
         
 
